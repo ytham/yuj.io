cf27a40a462a3ffd7b53d9e00ab947d8e0b13637
Yu Jiang Tham <ytham@apple.com>
2015-10-05 17:22:50 -0700

    Updated js and css

--- js/yuj.js
+++ js/yuj.js
@@ -2,8 +2,10 @@ $(document).ready(function () {
   var page = document.location.hash.split('#')[1]
   if (page === 'projects') {
     console.log("Projects Page");
+    // $('#basepage').css({ left: '100%' });
   } else if (page === 'contact') {
     console.log("Contact Page");
+    // $('#basepage').css({ left: '-100%' });
   }
 
   $('#link_projects').on('click', function () {
@@ -12,14 +14,26 @@ $(document).ready(function () {
 });
 
 function goToProjects() {
-  console.log("Project clicked");
-  $('#basepage').animate({ left: '100%', scrollTop: 0 });
+  $('#basepage').animate({ left: '0%', scrollTop: 0 });
+  $('#rightBackButton').css({ display: 'inline-block' });
 }
 
 function goToCover() {
-  $('#basepage').animate({ left: '0%', scrollTop: 0 });
+  $('#basepage').animate({ left: '-100%', scrollTop: 0 });
 }
 
 function goToContact() {
-  $('#basepage').animate({ left: '-100%', scrollTop: 0 });
+  $('#basepage').animate({ left: '-200%', scrollTop: 0 });
+  $('#leftBackButton').css({ display: 'inline-block' });
+}
+
+function backButton(position) {
+  if (position === 'left') {
+
+  } else if (position === 'right') {
+
+  } else {
+
+  }
+
 }
